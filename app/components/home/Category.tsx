26d6e76c663ba9352017e9d9fb36493bb6d3142b
sarpergulfirat <sarpergulfirat@gmail.com>
2023-12-26 01:02:31 +0300

    Folder structure set up, dynamic and static pages added, data fetched and displayed on the screen, and routing feature implemented.

---
+++ app/components/home/Category.tsx
@@ -0,0 +1,42 @@
+
+const Category = () => {
+ const categoryList = [
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  },
+  {
+   name: "Ayakkabı"
+  }
+ ]
+ return (
+  <div className="flex items-center justify-center px-3 md:px-10 gap-3 md:gap-10 py-5 md:py-8 overflow-x-auto">
+   {
+    categoryList.map((category, index) => (
+     <div className="border text-slate-500 rounded-full min-w-[120px] 
+     flex items-center justify-center flex-1 px-4 py-2 text-center cursor-pointer "
+      key={index}>{category.name}</div>
+    ))
+   }
+  </div>
+ )
+}
+
+export default Category
